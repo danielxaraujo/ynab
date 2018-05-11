5847792df0c5b45cfe2aff1b8d5a3ee35e1d3368
daniel.xavier@dataprev.gov.br <daniel.xavier@dataprev.gov.br>
2018-05-11 18:05:05 -0300

    Criando AccountForm

---
+++ frontend/src/views/account/AccountForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Container, Card, CardBody, Table, Badge } from 'reactstrap'
+
+class AccountForm extends Component {
+	render() {
+		return (
+			<Container fluid>
+			</Container>
+		)
+	}
+}
+
+export default AccountForm
