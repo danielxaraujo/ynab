6b8572c27954bd08eccbc3cead388b20a6700db9
Daniel Xavier Araujo <danielxaraujo@gmail.com>
2018-05-13 19:10:12 -0300

    Correção do BreadCrumb

--- frontend/src/containers/layout/BaseLayout.js
+++ frontend/src/containers/layout/BaseLayout.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { AppHeader, AppBreadcrumb, AppFooter } from '@coreui/react'
+import { AppHeader, AppFooter } from '@coreui/react'
 
 import { Router, routes } from '../../Router'
 
 import BaseHeader from './BaseHeader'
 import BaseSidebar from './BaseSidebar'
 import BaseFooter from './BaseFooter'
+import Breadcrumb from './Breadcrumb'
 
 class BaseLayout extends Component {
 	process
@@ -18,7 +19,7 @@ class BaseLayout extends Component {
 				<div className='app-body'>
 					<BaseSidebar {...this.props} />
 					<main className='main'>
-						<AppBreadcrumb appRoutes={routes} />
+						<Breadcrumb appRoutes={routes} />
 						<Router />
 					</main>
 				</div>
