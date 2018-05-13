badc7ab29b444eeccd90a2ff524ce0dab7d16165
Daniel Xavier Araujo <danielxaraujo@gmail.com>
2018-05-13 15:06:40 -0300

    criando tela de contas

--- frontend/src/views/account/Account.js
+++ frontend/src/views/account/Account.js
@@ -1,64 +1,68 @@
 import React, { Component } from 'react';
-import { Container, Card, CardBody, Table, Badge } from 'reactstrap'
+import Switch from 'react-switch'
+import { Container, Card, CardBody, Table, ButtonGroup, Button } from 'reactstrap'
 
 // Container style={{ backgroundColor: 'white', padding: '0px' }}
 //Table  style={{ margin: '0px' }}
+
+const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1YWY0ZjczMmE2NTUxMTI5NTY2NmFhMmYiLCJuYW1lIjoiRGFuaWVsIFhhdmllciBBcmHDumpvIiwiZW1haWwiOiJkYW5pZWx4YXJhdWpvQGdtYWlsLmNvbSIsInBhc3N3b3JkIjoiJDJhJDEwJGNPVWE5ekxJZEZuSk56UU14NWEuSWUwUlI0ZUg5L1FHWVdPMkJkSUM5cWMvREpteW5lR1BHIiwiX192IjowLCJpYXQiOjE1MjYyMjY0OTgsImV4cCI6MTUyNjMxMjg5OH0.M6nKK0NTOx0Q94Eh9V2XtCVfz545Ywg00iTZQrYt4kA'
+const URL = 'http://127.0.0.1:3000/api/account'
+
 class Account extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			accounts: []
+		};
+	}
+	componentDidMount() {
+		fetch(URL, {
+			headers: new Headers({
+				'Authorization': `Bearer ${TOKEN}`
+			})
+		}).then(response => (
+			response.json()
+		)).then(json => (
+			this.setState({ accounts: json.data })
+		))
+	}
 	render() {
+		let accounts = this.state.accounts
 		return (
 			<Container fluid>
 				<Card>
 					<CardBody>
-						<Table responsive striped hover>
+						<Table responsive striped bordered={false}>
 							<thead>
 								<tr>
-									<th>Username</th>
-									<th>Date registered</th>
-									<th>Role</th>
-									<th>Status</th>
+									<th>Icon</th>
+									<th>Name</th>
+									<th>Type</th>
+									<th>Budget</th>
+									<th></th>
 								</tr>
 							</thead>
 							<tbody>
-								<tr>
-									<td>Samppa Nori</td>
-									<td>2012/01/01</td>
-									<td>Member</td>
-									<td>
-										<Badge color='success'>Active</Badge>
-									</td>
-								</tr>
-								<tr>
-									<td>Estavan Lykos</td>
-									<td>2012/02/01</td>
-									<td>Staff</td>
-									<td>
-										<Badge color='danger'>Banned</Badge>
-									</td>
-								</tr>
-								<tr>
-									<td>Chetan Mohamed</td>
-									<td>2012/02/01</td>
-									<td>Admin</td>
-									<td>
-										<Badge color='secondary'>Inactive</Badge>
-									</td>
-								</tr>
-								<tr>
-									<td>Derick Maximinus</td>
-									<td>2012/03/01</td>
-									<td>Member</td>
-									<td>
-										<Badge color='warning'>Pending</Badge>
-									</td>
-								</tr>
-								<tr>
-									<td>Friderik Dávid</td>
-									<td>2012/01/21</td>
-									<td>Staff</td>
-									<td>
-										<Badge color='success'>Active</Badge>
-									</td>
-								</tr>
+								{accounts.map((account, idx) => {
+									return (
+										<tr id={idx}>
+											<td><i className={'fas fa-money-check-alt fa-lg fa-fw'}></i></td>
+											<td>{account.name}</td>
+											<td>{account.type}</td>
+											<td><Switch checked={account.budget} height={20} width={40} /></td>
+											<td>
+												<ButtonGroup>
+													<Button size='sm' color='success'>
+														<i className={'fas fa-edit fa-fw'}></i>
+													</Button>
+													<Button size='sm' color='danger'>
+														<i className={'fas fa-trash-alt fa-fw'}></i>
+													</Button>
+												</ButtonGroup>
+											</td>
+										</tr>
+									)
+								})}
 							</tbody>
 						</Table>
 					</CardBody>
